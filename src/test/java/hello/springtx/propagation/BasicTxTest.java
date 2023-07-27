cc4ad3eed6e371d8c5f96ce7214cf7f0aeaa1ebb
Min Seo Han <65328995+nexmin0805@users.noreply.github.com>
2023-07-27 09:16:45 +0900

    내부 트랜직션 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -84,7 +84,37 @@ public class BasicTxTest {
         TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
         log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);//무시하고 넘어감
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+
+    }
+
+    @Test
+    void outer_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
         txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+
+    }
+
+    @Test
+    void inner_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
 
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer);
