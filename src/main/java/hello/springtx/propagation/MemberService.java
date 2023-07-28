c51e2acaf54f7c2085007be1b1d07eb6c5c4384e
Min Seo Han <65328995+nexmin0805@users.noreply.github.com>
2023-07-28 10:52:01 +0900

    memberRepository에 저장

---
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -0,0 +1,22 @@
+package hello.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        memberRepository.save(member);
+        logRepository.save(logMessage);
+    }
+}
